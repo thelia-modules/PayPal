c5c85dd8962ac30edd442012dea2bb83f5deab40
Benjamin Perche <bperche9@gmail.com>
2014-02-12 09:21:31 +0100

    new file:   I18n/en_US.php 	new file:   I18n/fr_FR.php

---
+++ I18n/en_US.php
@@ -0,0 +1,24 @@
+<?php
+
+return array(
+    ' doesn\'t exist. You have to create it in order to use this module. Please see module\'s configuration page.' => ' doesn\'t exist. You have to create it in order to use this module. Please see module\'s configuration page.',
+    'Activate sandbox mode' => 'Activate sandbox mode',
+    'Can\'t read directory ' => 'Can\'t read directory ',
+    'Can\'t read file' => 'Can\'t read file',
+    'Can\'t read file ' => 'Can\'t read file ',
+    'Can\'t write directory ' => 'Can\'t write directory ',
+    'Can\'t write file' => 'Can\'t write file',
+    'Can\'t write file ' => 'Can\'t write file ',
+    'Discount' => 'Discount',
+    'Please change the rights on the file and/or directory.' => 'Please change the rights on the file and/or directory.',
+    'Please change the rights on the file.' => 'Please change the rights on the file.',
+    'The file ' => 'The file ',
+    'The file is corrupted.' => 'The file is corrupted.',
+    'The password option must be set.' => 'The password option must be set.',
+    'The signature option must be set.' => 'The signature option must be set.',
+    'The username option must be set.' => 'The username option must be set.',
+    'This directory doesn\'t exists and Paypal plugin directory isn\'t writable: ' => 'This directory doesn\'t exists and Paypal plugin directory isn\'t writable: ',
+    'login' => 'username',
+    'password' => 'password',
+    'signature' => 'signature',
+);
---
+++ I18n/fr_FR.php
@@ -0,0 +1,24 @@
+<?php
+
+return array(
+    ' doesn\'t exist. You have to create it in order to use this module. Please see module\'s configuration page.' => 'n\'existe pas. Vous devez le créer pour pouvoir utiliser ce module. Veuillez tout d\'abord accéder à la page de configuration de ce module',
+    'Activate sandbox mode' => 'Activer la sandbox ( mode de test )',
+    'Can\'t read directory ' => 'Le dossier suivant ne peut être lu',
+    'Can\'t read file' => 'Le fichier suivant ne peut être lu',
+    'Can\'t read file ' => 'Le fichier ne peut être lu',
+    'Can\'t write directory ' => 'Le dossier suivant ne peut être écrit',
+    'Can\'t write file' => 'Le fichier suivant ne peut être écrit',
+    'Can\'t write file ' => 'Le fichier ne peut être écrit',
+    'Discount' => 'Remise',
+    'Please change the rights on the file and/or directory.' => 'Veuillez changer les droits d\'accès au fichier/dossier',
+    'Please change the rights on the file.' => 'Veuillez changer les droits d\'accès au fichier',
+    'The file ' => 'Le fichier',
+    'The file is corrupted.' => 'Le fichier est corrompu',
+    'The password option must be set.' => 'Veuillez indiquer le mot de passe',
+    'The signature option must be set.' => 'Veuillez indiquer la signature',
+    'The username option must be set.' => 'Veuillez indiquer le nom d\'utilisateur',
+    'This directory doesn\'t exists and Paypal plugin directory isn\'t writable: ' => 'Le dossier n\'existe pas, et le dossier Paypal n\'est pas accessible en écriture: ',
+    'login' => 'Nom d\'utilisateur',
+    'password' => 'Mot de passe',
+    'signature' => 'Signature',
+);
