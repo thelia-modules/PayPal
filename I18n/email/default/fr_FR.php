b9e9f57d28eab9c26ca87a2523c1bb1600cdcfe3
Franck Allimant <github@allimant.org>
2016-01-12 01:21:59 +0100

    Fixed french translation

--- I18n/email/default/fr_FR.php
+++ I18n/email/default/fr_FR.php
@@ -7,6 +7,6 @@ return array(
     'The %store_name team.' => 'L\'équipe %store_name',
     'The payment of your order %ref is confirmed' => 'Le paiement Paypal de votre commande %ref est confirmé.',
     'This is a confirmation of the payment of your order %ref via Paypal on our shop.' => 'Ceci est une confirmation du paiement Paypal de votre commande %ref.',
-    'View this order in your account at %shop_name' => 'Les détails de cette commande sont dans votre compte client sur %shop_name',
-    'Your invoice is now available in your customer account at %url.' => 'Les détails de cette commande sont dans votre compte client sur %url',
+    'View this order in your account at %shop_name' => 'Les détails de cette commande sont disponibles dans votre compte client sur %shop_name',
+    'Your invoice is now available in your customer account at %url.' => 'Les détails de cette commande sont disponibles dans votre compte client sur %url',
 );
