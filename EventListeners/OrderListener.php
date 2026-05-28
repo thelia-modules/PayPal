7931a0f5d076da93aee9762193aa57502c9a5aac
hugo buffard <hugo.buffard@api-and-you.com>
2026-05-28 14:31:14 +0200

    FIX BUG : Passage commande cagnotte FO -> Warning: Undefined array key thelia_order_payment

--- EventListeners/OrderListener.php
+++ EventListeners/OrderListener.php
@@ -138,7 +138,8 @@ class OrderListener implements EventSubscriberInterface
         $payPalCartEvent = new PayPalCartEvent($this->payPalPaymentService->getCurrentPayPalCart());
         $this->dispatcher->dispatch($payPalCartEvent, PayPalEvents::PAYPAL_CART_DELETE);
 
-        $postedData = $this->requestStack->getCurrentRequest()->request->all()['thelia_order_payment'];
+        $allPosted = $this->requestStack->getCurrentRequest()->request->all();
+        $postedData = $allPosted['thelia_order_payment'] ?? $allPosted['apyfundjar_payment_form'] ?? [];
 
         if (isset($postedData[PayPalFormFields::FIELD_PAYMENT_MODULE]) && PayPal::getModuleId() === $event->getOrder()->getPaymentModuleId()) {
             $this->usePayPalMethod($postedData);
